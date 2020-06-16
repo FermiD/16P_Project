bfd15b24dac06159c41e688f2e118c0b04ce0cad
FermiD <55369111+FermiD@users.noreply.github.com>
2020-06-16 21:06:56 +0700

    Update, now with 16 (!) questions.
    Made 10 as the modifier for each questions add and reduce.
    Some questions altered in if statements since they can ask differently from patterns. Example: Q12

--- New Version (OOP)/OwenAqshaMatt_DSAProject_PersonalityTest.cpp
+++ New Version (OOP)/OwenAqshaMatt_DSAProject_PersonalityTest.cpp
@@ -70,8 +70,8 @@ int main(){
 		cin >> userAnswer;
 		
 		if(userAnswer == "a"){
-			i.decrease(25);
-			e.increase(25);
+			i.decrease(10);
+			e.increase(10);
 			break;
 		}	
 		else if(userAnswer == "b"){
@@ -79,8 +79,8 @@ int main(){
 			break;
 		}
 		else if(userAnswer == "c"){
-			i.increase(25);
-			e.decrease(25);
+			i.increase(10);
+			e.decrease(10);
 			break;
 		}
 		else{
@@ -99,8 +99,8 @@ int main(){
 		cin >> userAnswer;
 	
 		if(userAnswer == "a"){
-			n.decrease(25);
-			s.increase(25);
+			n.decrease(10);
+			s.increase(10);
 			break;
 		}	
 		else if(userAnswer == "b"){
@@ -108,8 +108,8 @@ int main(){
 			break;
 		}
 		else if(userAnswer == "c"){
-			n.increase(25);
-			s.decrease(25);
+			n.increase(10);
+			s.decrease(10);
 			break;
 		}
 		else{
@@ -128,8 +128,8 @@ int main(){
 		<< "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
 		cin >> userAnswer;
 		if(userAnswer == "a"){
-			f.decrease(25);
-			t.increase(25);
+			f.decrease(10);
+			t.increase(10);
 			break;
 		}	
 		else if(userAnswer == "b"){
@@ -137,8 +137,8 @@ int main(){
 			break;
 		}
 		else if(userAnswer == "c"){
-			f.increase(25);
-			t.decrease(25);
+			f.increase(10);
+			t.decrease(10);
 			break;
 		}
 		else{
@@ -153,12 +153,12 @@ int main(){
 	
 	// QUESTION 4
 	do{
-		cout<<"You judge morally aligned people openly instead of using discreet perception." << endl\
+		cout<<"Statement: You judge morally aligned people openly instead of using discreet perception." << endl\
 		<< "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
 		cin>>userAnswer;
 		if(userAnswer == "a"){
-			p.decrease(25);
-			j.increase(25);
+			p.decrease(10);
+			j.increase(10);
 			break;
 		}	
 		else if(userAnswer == "b"){
@@ -166,8 +166,65 @@ int main(){
 			break;
 		}
 		else if(userAnswer == "c"){
-			p.increase(25);
-			j.decrease(25);
+			p.increase(10);
+			j.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while(!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	// QUESTION 5
+	
+	do{
+		cout << "Statement: You heartfully enjoy gatherings or parties." << endl << "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin >> userAnswer;
+		
+		if(userAnswer == "a"){
+			i.decrease(10);
+			e.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "c"){
+			i.increase(10);
+			e.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while (!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// QUESTION 6
+	do{
+		cout << "Statement: You like to contemplate the impacts of your actions." << endl << "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin >> userAnswer;
+	
+		if(userAnswer == "a"){
+			n.decrease(10);
+			s.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "c"){
+			n.increase(10);
+			s.decrease(10);
 			break;
 		}
 		else{
@@ -180,6 +237,294 @@ int main(){
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	
+	// QUESTION 7
+	do{
+		cout << "Statement: You seek the reason behind a trend." << endl\
+		<< "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin >> userAnswer;
+		if(userAnswer == "a"){
+			f.decrease(10);
+			t.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "c"){
+			f.increase(10);
+			t.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while (!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// QUESTION 8
+	do{
+		cout<<"Statement: You are prepared for the worst, rather than leaving a good impression." << endl\
+		<< "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin>>userAnswer;
+		if(userAnswer == "a"){
+			p.decrease(10);
+			j.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "c"){
+			p.increase(10);
+			j.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while(!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// QUESTION 9
+	
+	do{
+		cout << "Statement: You embrace friendships no matter the differences between." << endl << "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin >> userAnswer;
+		
+		if(userAnswer == "a"){
+			i.decrease(10);
+			e.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "c"){
+			i.increase(10);
+			e.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while (!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// QUESTION 10
+	do{
+		cout << "Statement: You don't stray too far from society's norms." << endl << "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin >> userAnswer;
+	
+		if(userAnswer == "a"){
+			n.decrease(10);
+			s.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "c"){
+			n.increase(10);
+			s.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while(!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// QUESTION 11
+	do{
+		cout << "Statement: You often ask, why do people entrust themselves upon something?" << endl\
+		<< "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin >> userAnswer;
+		if(userAnswer == "a"){
+			f.decrease(10);
+			t.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "c"){
+			f.increase(10);
+			t.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while (!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// QUESTION 12
+	do{
+		cout<<"Statement: You would rather experience mistakes in learning, instead of avoiding it." << endl\
+		<< "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin>>userAnswer;
+		if(userAnswer == "c"){
+			p.decrease(10);
+			j.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "a"){
+			p.increase(10);
+			j.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while(!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// QUESTION 13
+	
+	do{
+		cout << "Statement: You are okay having a different opinion in topics, even when others do not." << endl << "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin >> userAnswer;
+		
+		if(userAnswer == "c"){
+			i.decrease(10);
+			e.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "a"){
+			i.increase(10);
+			e.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while (!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// QUESTION 14
+	do{
+		cout << "Statement: Your daydreams are useful." << endl << "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin >> userAnswer;
+	
+		if(userAnswer == "c"){
+			n.decrease(10);
+			s.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "a"){
+			n.increase(10);
+			s.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while(!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// QUESTION 15
+	do{
+		cout << "Statement: If many in a group expresses an emotion, you follow too." << endl\
+		<< "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin >> userAnswer;
+		if(userAnswer == "c"){
+			f.decrease(10);
+			t.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "a"){
+			f.increase(10);
+			t.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while (!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// QUESTION 16
+	do{
+		cout<<"Statement: In a groupwork, you let co-workers improvise instead of yourself setting standards." << endl\
+		<< "a. Very Much, b. Neutral, c. Definitely Not" << endl << "Your answer: ";
+		cin>>userAnswer;
+		if(userAnswer == "a"){
+			p.decrease(10);
+			j.increase(10);
+			break;
+		}	
+		else if(userAnswer == "b"){
+			// do nothing
+			break;
+		}
+		else if(userAnswer == "c"){
+			p.increase(10);
+			j.decrease(10);
+			break;
+		}
+		else{
+			cout << "Invalid answer." << endl << endl;
+		}
+	} while(!validateAnswer(userAnswer));
+	
+	userAnswer = "";
+	system("cls");
 	// REGION: Results Printing
 	
 	// Instantiate a Personality object.
